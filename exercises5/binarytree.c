47d719a059759ee102ae8f065ec1c4e30f7b2579
Stolof <olof.bergm@gmail.com>
2018-06-14 17:08:21 +0200

    binarytree

--- exercises5/binarytree.c
+++ exercises5/binarytree.c
@@ -13,7 +13,7 @@ typedef struct node Node;
 
 // b) return 1 if found value
 int research(struct node *root, int value){
-  struct node *current = root;
+  Node *current = root;
   while(current != NULL){
     if (current->key == value) return 1;
     else if ( current->key > value) current = current->leftNode;
@@ -33,29 +33,36 @@ int plusPetit(struct node *root){
   return root->key;
 }
 
-// d) WE CAN USE b) IN THIS ONE???
+// d)
 struct node *insert(struct node *root, int value){
-  printf("Time to insert the a node: root value is %d  value is %d\n", root->key, value);
-  struct node *current = root;
-  struct node *newNode = malloc(sizeof(Node));
+
+  Node *current = root;
+  Node *newNode = malloc(sizeof(Node));
   newNode->key = value;
   newNode->leftNode = NULL;
   newNode->rightNode = NULL;
+
   if(current == NULL){
     return newNode;
   }
   else {
+
     while(current != NULL){
       if (current->key > newNode->key && current->leftNode == NULL){
         current->leftNode = newNode;
         break;
       }
-      else if (current->key > newNode->key) current = current->leftNode;
-      else if (current->key < newNode->key && current->rightNode == NULL){
+      else if (current->key > newNode->key) {
+        current = current->leftNode;
+
+      }
+      else if (current->key < newNode->key && (current->rightNode == NULL)){
         current->rightNode = newNode;
         break;
       }
-      else if (current->key < newNode->key ) current = current->rightNode;
+      else if (current->key < newNode->key ){
+        current = current->rightNode;
+      }
     }
   }
   return root;
@@ -64,8 +71,11 @@ struct node *insert(struct node *root, int value){
 // e) return sorted values.
 int main(int argc, char const *argv[]){
 
-    struct node *root = NULL;
-    root = insert(root, 12);
+    Node *root = malloc(sizeof(Node));
+    root->key = 12;
+    root->rightNode = NULL;
+    root->leftNode = NULL;
+    root = insert(root, 13);
     root = insert(root, 5);
     root = insert(root, 7);
     root = insert(root, 10);
@@ -74,6 +84,6 @@ int main(int argc, char const *argv[]){
     printf("The higest value is: %d\n", plusGrand(root));
     printf("The lowest value is: %d\n", plusPetit(root));
     printf("Is 10 available in the tree %d\n", research(root, 10));
-    printf("Is 0 available in the tree  %d\n", research(root, 9));
+    printf("Is 9 available in the tree  %d\n", research(root, 9));
     return 0;
 }
